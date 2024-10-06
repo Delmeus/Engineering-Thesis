2a4907ba515c9c97d0313167f1aed0d662744c19
Delmeus <antektoczynski@gmail.com>
2024-10-06 16:04:35 +0200

    Utility scripts for creating train and test sets

---
+++ utils/binaryClassification/assess_train_test_sets.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+import pandas as pd
+
+def check_sickness_status(directory, csv_file, output_file):
+    df = pd.read_csv(csv_file)
+
+    df['image'] = df['image'].apply(lambda x: os.path.splitext(x)[0])
+
+    with open(output_file, 'w') as f:
+        f.write('filename,healthy\n')
+
+        for filename in os.listdir(directory):
+            file_without_ext = os.path.splitext(filename)[0]
+
+            if file_without_ext in df['image'].values:
+                healthy_status = df.loc[df['image'] == file_without_ext, 'healthy'].values[0]
+                f.write(f'{file_without_ext},{healthy_status}\n')
+
+    print(f"Output written to {output_file}")
+
+csv_file = '/img/archive/binaryProblem.csv'
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <source_directory> <destination_directory>")
+        return
+
+    dir = sys.argv[1]
+    output_file = dir + '/' + sys.argv[2]
+
+    if not os.path.exists(dir):
+        print("Podana sciezka nie istnieje lub nie jest katalogiem")
+        return
+
+    check_sickness_status(dir, csv_file, output_file)
+
+main()
---
+++ utils/binaryClassification/train_test_sets_info.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def print_healthy_percentage(df, type : str = ""):
+    total_images = len(df)
+    healthy_images = df['healthy'].sum()
+    percentage_healthy = (healthy_images / total_images) * 100
+    print(f"Percentage of healthy images in {type} files: {percentage_healthy:.2f}% and there are {total_images} images")
+
+train_values = '../../img/archive/train/train_values.csv'
+test_values = '../../img/archive/test/test_values.csv'
+
+df_train = pd.read_csv(train_values)
+df_test = pd.read_csv(test_values)
+
+print_healthy_percentage(df_train, "train")
+print_healthy_percentage(df_test, "test")
